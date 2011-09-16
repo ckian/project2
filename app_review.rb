b833f40f8c0de4e42059c1a300bfafbebfb2e612
ckian <ckian@mail.csuchico.edu>
2011-09-16 13:48:15 -0700

    working only with 'ruby app_review.rb reviews' if 'reviews' is a folder with .yml files in it

--- app_review.rb
+++ app_review.rb
@@ -1,46 +1,43 @@
 #!/usr/bin/env ruby
-
-
-
+require 'optparse'
 require 'yaml'
 
 a = []
 b = []
 
+origin = Dir.getwd
+
+Dir.chdir(ARGV[0])
 
 Dir.glob("*.yml").each do |rr|
 
-File.open(rr) do |f|
- YAML.load_documents(f) do |d|
+  File.open(rr) do |f|
 
-d.each do|g_or_b,content|
-content.each do|content2|
+    YAML.load_documents(f) do |d|
 
-if g_or_b == "good_apps"
+      d.each do|g_or_b,content|
+        content.each do|content2|
 
-a << "<tr><td>#{content2["url"]}</td><td>#{content2["name"]}</td><td>#{content2["note"]}</td></tr>"
+          if g_or_b == "good_apps"
+            a << "--><tr><td>#{content2["url"]}</td><td>#{content2["name"]}</td><td>#{content2["note"]}</td></tr><!--"
+           else
+            b << "--><tr><td>#{content2["url"]}</td><td>#{content2["name"]}</td><td>#{content2["note"]}</td></tr><!--"
+          end
 
-else
-
-b << "<tr><td>#{content2["url"]}</td><td>#{content2["name"]}</td><td>#{content2["note"]}</td></tr>"
-
+        end
+      end
+    end
+  end
 end
 
+Dir.chdir(origin)
 
 File.open("app_reviews.html", "w") { |aFile|
-
-aFile << "<html>\n<body><h2><center>Good Apps</center></h2>\n<table border = 1>\n\n<tr>\n<td>URLs</td>\n<td>Applications</td>\n<td>Reviews</td>\n</tr>\n"
-
-aFile << "#{a.sort}\n\n</table><BR><BR>"
-aFile << "<hr size=4 width=85%><br><BR><h2><center>Bad Apps</center></h2><table border = 1>"
-
-aFile << "#{b.sort}\n\n</table>\n</body>\n</html>"
-
+  aFile << "<html>\n<body><h2><center>Good Apps</center></h2>\n<table border = 1>"
+  aFile << "\n\n<tr>\n<td>URLs</td>\n<td>Applications</td>\n<td>Reviews</td>\n</tr>\n"
+  aFile << "<!--#{a.sort.uniq}-->\n\n</table><BR><BR><BR>"
+  aFile << "<hr size=4 width=85%><h2><center>Bad Apps</center></h2><table border = 1>"
+  aFile << "<!--#{b.sort.uniq}-->\n\n</table>\n</body>\n</html>"
 }
 
-end
-end
-end
-end
 puts "Generated app_reviews.html"
-end
